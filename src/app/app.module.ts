b9ef02ffebf78efe1aea0b528ddad84cb7e39ab5
rkfr <e.xbyvnqn@gmail.com>
2019-12-15 22:26:50 +0200

    added project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { AuthorsListComponent } from './components/authors-list/authors-list.component';
+import { BookPageComponent } from './components/book-page/book-page.component';
+import { AuthorPageComponent } from './components/author-page/author-page.component';
+import { GenrePageComponent } from './components/genre-page/genre-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    AuthorsListComponent,
+    BookPageComponent,
+    AuthorPageComponent,
+    GenrePageComponent,
   ],
   imports: [
     BrowserModule,

8811ee6a4541eb988df0bc7d26f0564000b37f7c
rkfr <e.xbyvnqn@gmail.com>
2019-12-15 23:28:25 +0200

    added genre component

--- src/app/components/genre-page/genre-page.component.ts
+++ src/app/components/genre-page/genre-page.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SharedService } from '../../services/shared.service';
+import { Book } from '../../services/Book.interface';
+
 @Component({
   selector: 'app-genre-page',
   templateUrl: './genre-page.component.html',
   styleUrls: ['./genre-page.component.scss']
 })
 export class GenrePageComponent implements OnInit {
+  constructor(private shared: SharedService) { }
 
-  constructor() { }
+  private isLoading: boolean = true;
+
+  books: Book[];
+  displayedColumns: string[] = ['title', 'author'];
+  displayedGenres: string[];
 
   ngOnInit() {
-  }
+    const genre = this.shared.getGenre();
 
+    this.books = this.shared.books.filter((book: Book) => book.genre.includes(genre));
+    
+    this.displayedGenres = this.books.reduce((genres, book) => {
+      const { genre } = book;
+      genres = [...genre];
+      return genres;
+    }, [])
+
+    this.isLoading = false;
+  }
 }

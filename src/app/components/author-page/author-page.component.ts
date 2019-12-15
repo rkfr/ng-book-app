21b9d589c897ecddcbdd981458e89167d5993d88
rkfr <e.xbyvnqn@gmail.com>
2019-12-15 23:06:44 +0200

    added author-page component

--- src/app/components/author-page/author-page.component.ts
+++ src/app/components/author-page/author-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { SharedService } from '../../services/shared.service';
+import { Author } from '../../services/Author.interface';
 
 @Component({
   selector: 'app-author-page',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./author-page.component.scss']
 })
 export class AuthorPageComponent implements OnInit {
+  constructor(
+    private shared: SharedService,
+    private route: ActivatedRoute
+  ) { }
 
-  constructor() { }
+  private loading: boolean = true;
+
+  author: Author;
+  authorId: string = this.route.snapshot.params.id;
 
   ngOnInit() {
+    this.shared.getAuthor(this.authorId).subscribe((author: Author) => {
+      this.author = author;
+      this.loading = false;
+    });
   }
 
 }

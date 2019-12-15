67bf58211c2ace43f7ac1d745e091c793f8e0fe1
rkfr <e.xbyvnqn@gmail.com>
2019-12-15 23:33:40 +0200

    routes were added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookPageComponent } from './components/book-page/book-page.component';
+import { AuthorsListComponent } from './components/authors-list/authors-list.component';
+import { AuthorPageComponent } from './components/author-page/author-page.component';
+import { GenrePageComponent } from './components/genre-page/genre-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'books', component: BookListComponent },
+  { path: 'books/:id', component: BookPageComponent },
+  { path: 'authors', component: AuthorsListComponent },
+  { path: 'authors/:id', component: AuthorPageComponent },
+  { path: 'genre', component: GenrePageComponent },
+  { path: '', redirectTo: './books', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

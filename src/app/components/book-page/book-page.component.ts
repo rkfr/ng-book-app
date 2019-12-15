1246ad7a62efa172e76cd6f7f1c0eb591307c606
rkfr <e.xbyvnqn@gmail.com>
2019-12-15 23:21:50 +0200

    added book component

--- src/app/components/book-page/book-page.component.ts
+++ src/app/components/book-page/book-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { SharedService } from '../../services/shared.service';
+import { Book } from '../../services/Book.interface';
 
 @Component({
   selector: 'app-book-page',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-page.component.scss']
 })
 export class BookPageComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private shared: SharedService
+  ) { }
 
-  constructor() { }
+  private loading: boolean = true;
+
+  book: Book;
+  bookId: string = this.route.snapshot.params.id;
 
   ngOnInit() {
+    this.shared.getBook(this.bookId).subscribe((book: Book) => {
+      this.book = book;
+      this.loading = false;
+    });
   }
 
+  onUpdateGenre(genre: string) {
+    this.shared.setGenre(genre);
+  }
 }
